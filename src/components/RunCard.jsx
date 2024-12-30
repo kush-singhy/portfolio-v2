408578b4156efb047bc5b2434418abff1f6a59d8
Kush <153984588+kush-singhy@users.noreply.github.com>
2024-12-30 21:52:22 +1100

    Run section mobile responsiveness

--- src/components/RunCard.jsx
+++ src/components/RunCard.jsx
@@ -10,9 +10,17 @@ export default function RunCard({ run, isLast }) {
   return (
     <>
       <div className="w-full flex flex-col md:flex-row justify-between px-5 py-4 hover:bg-gray-200 duration-200 gap-3">
-        <div>
-          <p className="font-medium truncate max-w-64">{run.name}</p>
-          <p className=" text-gray-500">{formatDate(run.start_date)}</p>
+        <div className="flex gap-4 md:flex-col md:gap-0">
+          <p className="font-medium truncate max-w-40 md:max-w-64">
+            {run.name}
+          </p>
+          <p className=" text-gray-500">
+            <span>{formatDate(run.start_date)}</span>
+            <span className="hidden md:inline">
+              {' '}
+              - {formatTime(run.start_date)}
+            </span>
+          </p>
         </div>
         <div className="flex gap-9">
           <div>
--- src/utils/formatUtils.js
+++ src/utils/formatUtils.js
@@ -9,20 +9,6 @@ export const formatPace = (meters, seconds) => {
   return `${minutes}'${remainingSeconds.toString().padStart(2, '0')}"`;
 };
 
-export const formatTime = (seconds) => {
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const remainingMinutes = minutes % 60;
-  const remainingSeconds = seconds % 60;
-
-  if (hours > 0) {
-    return `${hours}:${remainingMinutes
-      .toString()
-      .padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
-  }
-  return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
-};
-
 export const formatDate = (dateString) => {
   const date = new Date(dateString);
   const months = [
@@ -42,10 +28,16 @@ export const formatDate = (dateString) => {
   const day = date.getDate();
   const month = months[date.getMonth()];
   const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+};
+
+export const formatTime = (dateString) => {
+  const date = new Date(dateString);
   const hours = date.getHours();
   const minutes = date.getMinutes().toString().padStart(2, '0');
   const ampm = hours >= 12 ? 'pm' : 'am';
   const formattedHours = hours % 12 || 12;
 
-  return `${day} ${month} ${year} - ${formattedHours}:${minutes}${ampm}`;
+  return `${formattedHours}:${minutes}${ampm}`;
 };
