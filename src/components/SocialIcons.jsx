396edef795040c8a2ad8edbee059627eb7184cd8
Kush <153984588+kush-singhy@users.noreply.github.com>
2025-02-22 21:24:35 +1100

    Refining UI design

---
+++ src/components/SocialIcons.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import { FaMediumM } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { FiGithub } from 'react-icons/fi';
+import { FiLinkedin } from 'react-icons/fi';
+import { FiYoutube } from 'react-icons/fi';
+
+export default function SocialIcons() {
+  return (
+    <div id="social-icons" className="flex gap-2">
+      <SocialIcon
+        icon={<FiGithub className="w-5 h-5" />}
+        url="https://github.com/kush-singhy"
+      />
+      <SocialIcon
+        icon={<FiLinkedin className="w-5 h-5" />}
+        url="https://www.linkedin.com/in/kush-singh-yadav/"
+      />
+      <SocialIcon
+        icon={<FiYoutube className="w-5 h-5" />}
+        url="https://www.youtube.com/@kushsinghy"
+      />
+      <SocialIcon
+        icon={<FaXTwitter className="w-5 h-5" />}
+        url="https://x.com/kushsinghy_"
+      />
+      <SocialIcon
+        icon={<FaMediumM className="w-5 h-5" />}
+        url="https://medium.com/@kushsinghy"
+      />
+    </div>
+  );
+}
+
+export function SocialIcon({ icon, url }) {
+  const handleClick = () => {
+    window.open(url, '_blank');
+  };
+
+  return (
+    <div
+      className={`w-8 h-8 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300 transition-all duration-200`}
+      onClick={handleClick}
+    >
+      {icon}
+    </div>
+  );
+}
+
+SocialIcon.propTypes = {
+  icon: PropTypes.node.isRequired,
+  url: PropTypes.string.isRequired,
+  hoverColor: PropTypes.string.isRequired,
+};
--- src/sections/Details.jsx
+++ src/sections/Details.jsx
@@ -1,12 +1,9 @@
-import SocialIcon from '../components/SocialIcon';
 import TechIcon from '../components/TechIcon';
 import heroImage from '../assets/headshot.webp';
+import SocialIcons from '../components/SocialIcons';
 import { FiDownloadCloud } from 'react-icons/fi';
 import { FiMapPin } from 'react-icons/fi';
-import { FiGithub } from 'react-icons/fi';
-import { FiYoutube } from 'react-icons/fi';
-import { FaMediumM } from 'react-icons/fa';
-import { FiLinkedin } from 'react-icons/fi';
+
 import jsIcon from '../assets/javascript.svg';
 import reactIcon from '../assets/react.svg';
 import nodeIcon from '../assets/nodejs.svg';
@@ -29,11 +26,11 @@ export default function Details() {
         <img
           src={heroImage}
           alt="Kush Singhy"
-          className="rounded-full w-20 h-20 md:w-32 md:h-32 mb-4 md:mb-7"
+          className="rounded-full w-16 h-16 md:w-32 md:h-32 mb-4 md:mb-7"
         />
 
         <div className="flex flex-col gap-2">
-          <h1 className="text-4xl md:text-5xl font-bold text-gray-800 md:min-w-72 md:mb-2">
+          <h1 className="text-4xl font-bold text-gray-800 md:min-w-72 md:mb-2">
             Kush Singhy
           </h1>
           <div className="flex md:gap-3 mb-3">
@@ -46,7 +43,7 @@ export default function Details() {
             </div>
             <div className="flex mr-auto text-gray-500 min-w-40">
               <div
-                className="flex gap-2 items-center hover:scale-[1.02] hover:bg-gray-200 px-2 py-1 rounded-lg hover:text-gray-800 duration-200 cursor-pointer"
+                className="flex gap-2 items-center hover:bg-gray-300 px-2 py-1 rounded-lg hover:text-gray-800 duration-200 cursor-pointer"
                 onClick={handleCV}
               >
                 <FiDownloadCloud />
@@ -57,31 +54,12 @@ export default function Details() {
         </div>
       </div>
 
-      <div className="text-lg md:text-xl font-light text-gray-600 mb-7">
-        A software developer who likes to make cool apps, run long distances and
-        read good books.
+      <div className="text-lg text-gray-700 mb-4 leading-6">
+        A software developer who likes to make cool apps and run long distances.
       </div>
 
       <div id="socials" className="mb-7">
-        <p className="text-gray-800 mb-2">Find me here</p>
-        <div id="social-icons" className="flex gap-2">
-          <SocialIcon
-            icon={<FiGithub className="w-5 h-5" />}
-            url="https://github.com/kush-singhy"
-          />
-          <SocialIcon
-            icon={<FiYoutube className="w-5 h-5" />}
-            url="https://www.youtube.com/@kushsinghy"
-          />
-          <SocialIcon
-            icon={<FiLinkedin className="w-5 h-5" />}
-            url="https://www.linkedin.com/in/kush-singhy/"
-          />
-          <SocialIcon
-            icon={<FaMediumM className="w-5 h-5" />}
-            url="https://medium.com/@kushsinghy02"
-          />
-        </div>
+        <SocialIcons />
       </div>
 
       <div id="tech-stack" className="mb-16">
